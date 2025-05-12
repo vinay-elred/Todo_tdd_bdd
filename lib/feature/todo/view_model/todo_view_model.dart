fa2d08a63a81cba80a40d85738be3c88eef57a83
Vinay_Sharma <vinay@elred.io>
2025-05-12 12:50:28 +0530

    test: Todo ViewModel - fetch Todos retuen success - passed

--- lib/feature/todo/view_model/todo_view_model.dart
+++ lib/feature/todo/view_model/todo_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/feature/todo/model/todo_model.dart';
 import 'package:todo_app/feature/todo/repository/todo_repository.dart';
@@ -9,7 +11,14 @@ class TodoViewModel extends ChangeNotifier {
   List<TodoModel> _todos = [];
   List<TodoModel> get todos => _todos;
 
-  Future<void> fetch() async {}
+  Future<void> fetch() async {
+    final response = await repository.fetch();
+    response.fold(
+      (failure) => log("Fetch failed $failure"),
+      (todos) => _todos = [...todos],
+    );
+    notifyListeners();
+  }
 
   Future<void> add(TodoModel todo) async {}
 }
